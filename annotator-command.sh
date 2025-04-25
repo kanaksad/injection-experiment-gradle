0788374b9a9fa82e0550aaa732b3e5dfd2e52f36
Kanak Das <kdas006@ucr.edu>
2025-04-24 17:38:39 -0700

    configs

--- annotator-command.sh
+++ annotator-command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ANNOTATOR_TYPE_ARG=true ANNOTATOR_POLY=true ANNOTATOR_LIBRARY=true UCRT_VERSION=0.3-SNAPSHOT ./gradlew $1:compileJava
+ANNOTATOR_TYPE_ARG=true ANNOTATOR_POLY=true ANNOTATOR_LIBRARY=true UCRT_VERSION=0.3-SNAPSHOT-securibench ./gradlew clean $1:compileJava
